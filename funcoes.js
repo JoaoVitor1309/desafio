921779789da4b87aee9db4f7d50aa08d0a51f00c
JoãoVitor1309 <joaovitorbianchini09@gmail.com>
2022-04-08 11:23:57 -0300

    desafio

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+/*funcoes.js*/
+function calculaidade(anonasc) {
+  return 2022 - (anonasc);
+}
+module.exports.calculaidade = calculaidade;
+
+module.exports.calculaimc = function (peso, altura) {
+    return peso / (altura * altura);
+  }
